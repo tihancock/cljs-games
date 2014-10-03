23c5649a2021118e50c38abb593074e748190cd1
Tom Hancock <thomas.ian.hancock@gmail.com>
2014-10-02 19:59:04 -0600

    Actually *use* reagent properly (d'oh!)

--- picture-matching/src/picture_matching/core.cljs
+++ picture-matching/src/picture_matching/core.cljs
@@ -1,52 +1,58 @@
 (ns picture-matching.core
   (:require [reagent.core :as r]))
 
+(def images [{:id 1  :path "resources/1.jpg" :displayed true}
+             {:id 2  :path "resources/1.jpg" :displayed true}
+             {:id 3  :path "resources/2.jpg" :displayed true}
+             {:id 4  :path "resources/2.jpg" :displayed true}
+             {:id 5  :path "resources/3.jpg" :displayed true}
+             {:id 6  :path "resources/3.jpg" :displayed true}
+             {:id 7  :path "resources/4.jpg" :displayed true}
+             {:id 8  :path "resources/4.jpg" :displayed true}
+             {:id 9  :path "resources/5.jpg" :displayed true}
+             {:id 10 :path "resources/5.jpg" :displayed true}
+             {:id 11 :path "resources/6.jpg" :displayed true}
+             {:id 12 :path "resources/6.jpg" :displayed true}
+             {:id 13 :path "resources/7.jpg" :displayed true}
+             {:id 14 :path "resources/7.jpg" :displayed true}
+             {:id 15 :path "resources/8.jpg" :displayed true}
+             {:id 16 :path "resources/8.jpg" :displayed true}])
+
 (def app-state
-  (r/atom {:images ["resources/1.jpg"
-                    "resources/2.jpg"
-                    "resources/3.jpg"
-                    "resources/4.jpg"
-                    "resources/5.jpg"
-                    "resources/6.jpg"
-                    "resources/7.jpg"
-                    "resources/8.jpg"]}))
+  (r/atom {:images (shuffle images)}))
 
-(def positions
-  [[0 0 25 25]
-   [25 0 50 25]
-   [50 0 75 25]
-   [75 0 100 25]
-   [0 25 25 50]
-   [25 25 50 50]
-   [50 25 75 50]
-   [75 25 100 50]
-   [0 50 25 75]
-   [25 50 50 75]
-   [50 50 75 75]
-   [75 50 100 75]
-   [0 75 25 100]
-   [25 75 50 100]
-   [50 75 75 100]
-   [75 75 100 100]])
+(defn toggle-displayed
+  [image]
+  (assoc image :displayed (not (:displayed image))))
 
-(defn double-images [images]
-  (concat images images))
+(defn update-images!
+  [f]
+  (swap! app-state (fn [state] (assoc state :images (mapv f (:images state))))))
 
-(defn image [i l t r b]
-  [:div {:style {"position" "absolute"
-                 "left" (str l "%")
-                 "top" (str t "%")
-                 "right" (str r "%")
-                 "bottom" (str b "%")}}
-   [:img {:src i}]])
+(defn hide-all-images []
+  (update-images! #(assoc % :displayed false)))
+
+(defn image [i]
+  [:img {:on-click #(update-images! (fn [image] (if (= (:id i) (:id image))
+                                                  (toggle-displayed image)
+                                                  image)))
+         :style {"width" "25%"
+                 "height" "25%"
+                 "opacity" (if (:displayed i) 1.0 0.0)}
+         :src (:path i)}])
+
+(defn timer []
+  (fn []
+    (js/setTimeout hide-all-images 2000)
+    [:div]))
 
 (defn image-grid []
   [:div
-   [:ul
-    (let [randomised-images (-> (:images @app-state)
-                                double-images
-                                shuffle)]
-      (map (fn [i [l t r b]] [image i l t r b]) randomised-images positions))]])
+    [:ul
+     (for [image-row (partition 4 (:images @app-state))]
+       [:div {:style {"background" "black"}}
+        (map (fn [i] [image i]) image-row)])]
+   [timer]])
 
 (defn start []
   (r/render-component
